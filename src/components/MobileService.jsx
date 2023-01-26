ed41c18225b72cdfa06ad93909198e45a4d7edad
bkmahapatra <bkmahapatra27@gmail.com>
2023-01-26 10:52:56 +0530

    fix 11

---
+++ src/components/MobileService.jsx
@@ -0,0 +1,10 @@
+import Data from "../Data.json";
+const MobileService = () => {
+  return (
+    <section className="mobileservice" id="mobile-service">
+      
+    </section>
+  );
+};
+
+export default MobileService;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,20 +1,31 @@
 import Data from "../Data.json";
 import "../styles/Services.css";
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import Sub_services from "./Sub_services";
 import { useState } from "react";
+import {FaAngleRight} from "react-icons/fa";
 
 const Services = () => {
   const [cr_service, setCr_service] = useState("fertilitytreatment");
+  const [ser, setSer] = useState(false);
+  const [subSer, setSubSer] = useState(false);
 
   // console.log(cr_service);
 
   const handler = (e) => {
-    document.querySelectorAll(".services_items").forEach(item=>{
+    document.querySelectorAll(".services_items").forEach((item) => {
       item.classList.remove("active");
-    })
+    });
 
     e.currentTarget.classList.add("active");
+  };
+  const serHandler1= (e)=>{
+    document.querySelectorAll(".heading1").forEach(item=>{item.classList.remove("vactive")})
+    e.currentTarget.classList.add("vactive");
+  }
+  const serHandler2 = (e) => {
+    document.querySelectorAll(".sub-ser-view").forEach(item=>{item.classList.remove("sactive")})
+    e.currentTarget.classList.add("sactive");
+
   };
 
   return (
@@ -23,40 +34,86 @@ const Services = () => {
         {Data.landingPage.services_section.section_title}
       </h2>
       <div className="services_body">
-        <div className="services_menu">
-          {Data.landingPage.services_section.services.map((item, index) => {
-            return (
-              <div
-                className="services_items"
-                key={index}
-                onClick={(e) => handler(e)}
-              >
-                <div className="bar"></div>
+        <div className="webService">
+          <div className="services_menu">
+            {Data.landingPage.services_section.services.map((item, index) => {
+              return (
                 <div
-                  className="ser_item"
-                  onClick={() =>
-                    setCr_service(
-                      item.service_title.split(" ").join("").toLowerCase()
-                    )
-                  }
+                  className="services_items"
+                  key={index}
+                  onClick={(e) => handler(e)}
                 >
-                  {item.service_title}
+                  <div className="bar"></div>
+                  <div
+                    className="ser_item"
+                    onClick={() =>
+                      setCr_service(
+                        item.service_title.split(" ").join("").toLowerCase()
+                      )
+                    }
+                  >
+                    {item.service_title}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
 
-        <div className="services_details">
+          <div className="services_details">
+            {Data.landingPage.services_section.services.map((item, index) => {
+              return (
+                <div key={index}>
+                  {cr_service ===
+                  item.service_title.split(" ").join("").toLowerCase() ? (
+                    <Sub_services ser_data={item} />
+                  ) : (
+                    ""
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        {/* mobile */}
+        <div className="phoneService">
           {Data.landingPage.services_section.services.map((item, index) => {
             return (
-              <div key={index}>
-                {cr_service ===
-                item.service_title.split(" ").join("").toLowerCase() ? (
-                  <Sub_services ser_data={item} />
-                ) : (
-                  ""
-                )}
+              <div className="heading1" onClick={(e) =>serHandler1(e)}>
+                <div className="ser-mob-h1" key={index}>
+                  {item.service_title} <span className="dropicon1"><FaAngleRight/></span>
+                </div>
+
+                <div className="submob-ser">
+                  {item.sub_services.map((item, index) => {
+                    return (
+                      <div
+                        className="sub-ser-view"
+                        onClick={(e) => {
+                          serHandler2(e);
+                        }}
+                      >
+                        <div className="sub-mob-ser-h2" key={index}>
+                          {item.sub_service_name} <span className="dropicon2"><FaAngleRight/></span>
+                        </div>
+
+                        <div className="sub-ser-body">
+                          <img src={""} alt="" className="sub-item-img" />
+                          <div className="msub-item-title">
+                            {item.sub_service_title}
+                          </div>
+                          <div className="msub-item-desc">
+                            {item.sub_service_desc}
+                          </div>
+                          <ul className="msub-item-keys">
+                            {item.sub_service_keypoints.map((item, index) => {
+                              return <li className="msub-key-pts">{item}</li>;
+                            })}
+                          </ul>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             );
           })}
