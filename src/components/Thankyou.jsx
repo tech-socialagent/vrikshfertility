c9161efe5e5b0b260079ac4a8f4565c3192e166d
bkmahapatra <bkmahapatra27@gmail.com>
2023-03-06 16:32:25 +0530

    gtag fix 62

--- src/components/Thankyou.jsx
+++ src/components/Thankyou.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "../styles/Thankyou.css";
+import ReactGA from 'react-ga';
+
 
 const Thankyou = () => {
+  useEffect(()=>{
+    ReactGA.initialize('AW-11099289492');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
+  })
   return (
     <div className="thankyou_body">
         <h2>Thank You for submitting the form.</h2>
